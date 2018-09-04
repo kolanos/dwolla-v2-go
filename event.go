51ba40980448027ce11ee4c6acf1408e1e91df48
Michael Lavers <kolanos@gmail.com>
2018-09-03 21:05:28 -0700

    Adding webhook methods

--- event.go
+++ event.go
@@ -1,8 +1,16 @@
 package dwolla
 
+import (
+	"fmt"
+	"net/url"
+)
+
 // EventService is the event service interface
 // see: https://docsv2.dwolla.com/#events
-type EventService interface{}
+type EventService interface {
+	List(*url.Values) (*Events, error)
+	Retrieve(string) (*Event, error)
+}
 
 // EventServiceOp is an implementation of the event service interface
 type EventServiceOp struct {
@@ -26,4 +34,36 @@ type Events struct {
 	Collection
 	client   *Client
 	Embedded map[string][]Event `json:"_embedded"`
+}
+
+// List returns a collection of events
+// see: https://docsv2.dwolla.com/#list-events
+func (e *EventServiceOp) List(params *url.Values) (*Events, error) {
+	var events Events
+
+	if err := e.client.Get("events", params, nil, &events); err != nil {
+		return nil, err
+	}
+
+	events.client = e.client
+
+	for i := range events.Embedded["events"] {
+		events.Embedded["events"][i].client = e.client
+	}
+
+	return &events, nil
+}
+
+// Retrieve retrieves the event matching the id
+// see: https://docsv2.dwolla.com/#retrieve-an-event
+func (e *EventServiceOp) Retrieve(id string) (*Event, error) {
+	var event Event
+
+	if err := e.client.Get(fmt.Sprintf("events/%s", id), nil, nil, &event); err != nil {
+		return nil, err
+	}
+
+	event.client = e.client
+
+	return &event, nil
 }
--- webhook.go
+++ webhook.go
@@ -1,10 +1,131 @@
 package dwolla
 
+import (
+	"fmt"
+)
+
 // WebhookService is the webhook service interface
 // see: https://docsv2.dwolla.com/#webhooks
-type WebhookService interface{}
+type WebhookService interface {
+	Retrieve(string) (*Webhook, error)
+}
 
 // WebhookServiceOp is an implementation of the webhook service interface
 type WebhookServiceOp struct {
 	client *Client
+}
+
+// Webhook is a dwolla webhook
+type Webhook struct {
+	Resource
+	ID             string           `json:"id"`
+	Topic          EventTopic       `json:"topic"`
+	AccountID      string           `json:"accountId"`
+	EventID        string           `json:"eventId"`
+	SubscriptionID string           `json:"subscriptionId"`
+	Attempts       []WebhookAttempt `json:"attempts"`
+}
+
+// Webhooks is a collection of webhooks
+type Webhooks struct {
+	Collection
+	Embedded map[string][]Webhook `json:"_embedded"`
+	Total    int                  `json:"total"`
+}
+
+// WebhookAttempt is a webhook attempt
+type WebhookAttempt struct {
+	ID       string          `json:"id"`
+	Request  WebhookRequest  `json:"request"`
+	Response WebhookResponse `json:"response"`
+}
+
+// WebhookHeader is a webhook request/response header
+type WebhookHeader struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+// WebhookRequest is a webhook request
+type WebhookRequest struct {
+	Timestamp string          `json:"timestamp"`
+	UrL       string          `json:"url"`
+	Headers   []WebhookHeader `json:"headers"`
+	Body      string          `json:"body"`
+}
+
+// WebhookResponse is a webhook response
+type WebhookResponse struct {
+	Timestamp  string          `json:"timestamp"`
+	Headers    []WebhookHeader `json:"headers"`
+	StatusCode int             `json:"statusCode"`
+	Body       string          `json:"body"`
+}
+
+// WebhookRetry is a webhook retry
+type WebhookRetry struct {
+	Resource
+	ID        string `json:"id"`
+	Timestamp string `json:"timestamp"`
+}
+
+// WebhookRetries is a collection of webhook retries
+type WebhookRetries struct {
+	Collection
+	Embedded map[string][]WebhookRetry `json:"_embedded"`
+	Total    int                       `json:"total"`
+}
+
+// Retrieve retrieves the webhook with matching id
+// see: https://docsv2.dwolla.com/#retrieve-a-webhook
+func (w *WebhookServiceOp) Retrieve(id string) (*Webhook, error) {
+	var webhook Webhook
+
+	if err := w.client.Get(fmt.Sprintf("webhooks/%s", id), nil, nil, &webhook); err != nil {
+		return nil, err
+	}
+
+	webhook.client = w.client
+
+	return &webhook, nil
+}
+
+// ListRetries returns a collection of retries for this webhook
+// see: https://docsv2.dwolla.com/#list-retries-for-a-webhook
+func (w *Webhook) ListRetries() (*WebhookRetries, error) {
+	var retries WebhookRetries
+
+	if _, ok := w.Links["retry"]; !ok {
+		return nil, fmt.Errorf("No retry resource link")
+	}
+
+	if err := w.client.Get(w.Links["retry"].Href, nil, nil, &retries); err != nil {
+		return nil, err
+	}
+
+	retries.client = w.client
+
+	for i := range retries.Embedded["retries"] {
+		retries.Embedded["retries"][i].client = w.client
+	}
+
+	return &retries, nil
+}
+
+// Retry retries the webhook
+// see: https://docsv2.dwolla.com/#retry-a-webhook
+func (w *Webhook) Retry() (*WebhookRetry, error) {
+	var retry WebhookRetry
+
+	if _, ok := w.Links["retry"]; !ok {
+		return nil, fmt.Errorf("No retry resource link")
+	}
+
+	if err := w.client.Post(w.Links["retry"].Href, nil, nil, &retry); err != nil {
+		return nil, err
+	}
+
+	retry.client = w.client
+
+	return &retry, nil
 }
