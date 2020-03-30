9d901206629bbf3a8da83c823000c8ab8bf19854
Bryan Swords <bzswords@gmail.com>
2020-03-30 14:31:11 -0400

    Remove TokenURL and use APIURL for token requests (#5)
    * Remove TokenURL and use APIURL for token requests
    
    * readd TokenURL method and constants to preserve API continuity
    
    * fix typo in comments
    
    * tests

--- client.go
+++ client.go
@@ -31,6 +31,7 @@ const (
 	// ProductionAuthURL is the production auth url
 	ProductionAuthURL = "https://www.dwolla.com/oauth/v2/authenticate"
 	// ProductionTokenURL is the production token url
+	// Deprecated - use https://api.dwolla.com/token moving forward
 	ProductionTokenURL = "https://accounts.dwolla.com/token"
 
 	// SandboxAPIURL is the sandbox api url
@@ -38,6 +39,7 @@ const (
 	// SandboxAuthURL is the sandbox auth url
 	SandboxAuthURL = "https://sandbox.dwolla.com/oauth/v2/authenticate"
 	// SandboxTokenURL is the sandbox token url
+	// Deprecated - use https://api-sandbox.dwolla.com moving forward
 	SandboxTokenURL = "https://accounts-sandbox.dwolla.com/token"
 )
 
@@ -186,7 +188,7 @@ func (c *Client) RequestToken(ctx context.Context) error {
 
 	buf := bytes.NewBuffer([]byte("grant_type=client_credentials"))
 
-	req, err := http.NewRequest("POST", c.TokenURL(), buf)
+	req, err := http.NewRequest("POST", c.BuildAPIURL("token"), buf)
 	if err != nil {
 		return err
 	}
